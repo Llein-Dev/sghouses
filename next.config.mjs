28804adaa1f4ed82595fa32cae4de741c5c746b7
manhtuonq <stronggwall04@gmail.com>
2024-10-23 01:20:51 +0700

    Add_categories

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        domains: ['localhost'], // Thêm miền vào đây
+      },
+};
 
 export default nextConfig;

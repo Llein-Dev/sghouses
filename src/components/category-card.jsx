288efa56e7333f1b09157d1036c6fe3fb630645a
Lein <vohuudat282224@gmail.com>
2024-10-23 01:40:19 +0700

    push

--- src/components/category-card.jsx
+++ src/components/category-card.jsx
@@ -1,29 +1,29 @@
 "use client"
-import Image from "next/image";
+
 import Link from "next/link";
 
 export default function VerticalCategory({ categories }) {
     return (
         <>
-        <div className="grid grid-cols-2 md:grid-cols-6 container mx-auto justify-center gap-6 mb-12">  
-        {categories.map((category, index) => (
-                <Link key={index} href={`/categories/${category.id}`} className="flex w-full rounded-lg shadow flex-col items-center group">
-                    <div className="relative w-full aspect-[4/3] mb-2 overflow-hidden">
-                        <Image
-                            src={`${category.image}`}
-                            alt={category.name}
-                            fill
-                            style={{ objectFit: 'cover' }} // Sử dụng style thay cho objectFit
-                            className="transition-transform duration-300 group-hover:scale-110"
-                        />
-                    </div>
-                    <h3 className="text-sm font-medium text-center pb-3 group-hover:text-primary transition-colors">
-                        {category.name}
-                    </h3>
-                </Link>
-              ))}
-        </div>
-       
+            <div className="grid grid-cols-2 md:grid-cols-6 container mx-auto justify-center gap-6 mb-12">
+                {categories.map((category, index) => (
+                    <Link key={index} href={`/categories/${category.id}`} className="flex w-full rounded-lg shadow flex-col items-center group">
+                        <div className="relative w-full aspect-[4/3] mb-2 overflow-hidden">
+                            <img
+                                src={`${category.image}`}
+                                alt={category.name}
+                                fill
+                                style={{ objectFit: 'cover' }} // Sử dụng style thay cho objectFit
+                                className="transition-transform duration-300 group-hover:scale-110"
+                            />
+                        </div>
+                        <h3 className="text-sm font-medium text-center pb-3 group-hover:text-primary transition-colors">
+                            {category.name}
+                        </h3>
+                    </Link>
+                ))}
+            </div>
+
         </>
     );
 }

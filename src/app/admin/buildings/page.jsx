8a764d63d04c2b562526b8993dd8a9e09875f039
Mạnh Tường <stronggwall04@gmail.com>
2024-11-16 16:08:20 +0700

    task building update 90%

--- src/app/admin/buildings/page.jsx
+++ src/app/admin/buildings/page.jsx
@@ -109,7 +109,9 @@ export default function CategoryBlog() {
   const handleCreatPage = () => {
     router.push('/admin/buildings/create_building')
   }
-
+  const handleEditBuilding = (id) => {
+    router.push(`/admin/buildings/update/${id}`)
+  }
   return (
     <div className="space-y-4">
    <div className="flex justify-between items-center">
@@ -170,58 +172,10 @@ export default function CategoryBlog() {
                   {/* Nút Gọi điện */}
                   <Dialog>
                     <DialogTrigger asChild>
-                      <Button variant="outline" >
+                      <Button variant="outline"  onClick={() => handleEditBuilding(building.id)} >
                         <Pencil className="mr-2 h-4 w-4" />
                       </Button>
                     </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                      <DialogHeader>
-                        <DialogTitle>Edit Cate BLog</DialogTitle>
-                        <DialogDescription>
-                          Edit a Cate Blog account.
-                        </DialogDescription>
-                      </DialogHeader>
-                      <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                          <Label htmlFor="name" className="text-right">
-                            Name
-                          </Label>
-                          <Input
-                            id="name"
-                            // value={name}
-                            // onChange={(e) => setName(e.target.value)}
-                            className="col-span-3"
-                          />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                          <Label htmlFor="phone" className="text-right">
-                            Phone
-                          </Label>
-                          <Input
-                            id="phone"
-                            type="phone"
-                            // value={slug}
-                            // onChange={(e) => setSlug(e.target.value)}
-                            className="col-span-3"
-                          />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                          <Label htmlFor="born" className="text-right">
-                            Born
-                          </Label>
-                          <Input
-                            id="born"
-                            type="born"
-                            // value={status}
-                            // onChange={(e) => setStatus(e.target.value)}
-                            className="col-span-3"
-                          />
-                        </div>
-                      </div>
-                      <DialogFooter>
-                        <Button type="submit" >Add User</Button>
-                      </DialogFooter>
-                    </DialogContent>
                   </Dialog>
 
 
